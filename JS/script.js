9b8a633d33cc0537d662af313c7e0bd6fbdb98f2
AntaaaKD <soxnaanta844@gmail.com>
2024-10-22 22:02:46 +0000

    PTF final

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".typewriter-text", {
+    strings: [ // <-- changer 'string' à 'strings'
+        "___ Fondatrice de Sa Muse❤️___",
+        "a BlueTeamer🕵",
+        "a Crochet Enthusiast🧶",
+        "an Animes Lover🔥❤️",
+    ],
+    typeSpeed: 100,
+    backSpeed: 70,
+    loop: true,
+});
